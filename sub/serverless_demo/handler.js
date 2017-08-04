9fc3486dee657e5bb33d8cea8e6e050efe348979
Rich Burdon <rich.burdon@gmail.com>
2017-08-04 15:48:44 -0400

    Uses test webhook (in production).

--- sub/serverless_demo/handler.js
+++ sub/serverless_demo/handler.js
@@ -6,21 +6,23 @@ import request from 'request';
 
 module.exports.hello = (event, context, callback) => {
 
-  // TODO(burdon): Set API KEY in header.
-  // TODO(burdon): Simulate API Hook.
-  // curl -i -X POST https://app.robotik.io/hook/83CC572F-90DE-406F-ABFB-4C770C2381EB -H "Content-Type: application/json" -d ""
-
+  // TODO(burdon): Set API KEY in header and call GraphQL API.
   request.post({
-    url: `https://app.robotik.io/hook/${process.env['ALIEN_API_KEY']}`
+    url: `https://app.robotik.io/hook/${process.env['ALIEN_WEBHOOK']}`
   }, (error, response, body) => {
 
     // TODO(burdon): Error handling.
+    if (error) {
+      callback(error);
+      return;
+    }
+
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
     callback(null, {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Alien Serverless OK: ' + process.env['ALIEN_API_KEY'],
+        message: 'Alien OK',
         input: event
       }),
     });
--- sub/serverless_demo/scripts/deploy.sh
+++ sub/serverless_demo/scripts/deploy.sh
@@ -1,26 +1,36 @@
 #!/usr/bin/env bash
 
+AWS_PROFILE=alienlabs
+
 #
-# Build
+# Build.
 #
 
 sls webpack
 
 #
-# Calls CloudFormation
+# Calls CloudFormation.
 #
 
 sls deploy
 sls deploy --function hello
 
 #
-# Test
+# Test funciton.
 #
 
 sls invoke --function hello
 
 #
-# Show logs (from CloudWatch)
+# Test API.
+#
+
+API_URL="https://7u7jsipmfh.execute-api.us-east-1.amazonaws.com/dev/hello"
+
+curl -i -X POST ${API_URL} -H "Content-Type: application/json" -d "{}"
+
+#
+# Show logs (from CloudWatch).
 #
 
 sls logs --function hello --tail
